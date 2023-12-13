e27a4c77a554234b6357bd1344cc0849217e2115
tioatrik <nurainieee9@gmail,com>
2023-12-13 20:10:12 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".navbar-section-mobile");
+const hide = document.querySelector("body")
+
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  hide.classList.toggle("active");
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".navbar-section-mobile").forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
+  hide.classList.remove("active");
+
 }))
